a05c0416d9da090a999cb5af8a0cff17690470c7
Brahim Chaouki <bchaouki@peaks.fr>
2025-09-22 16:20:51 +0200

    initial commit

---
+++ apps/api/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: [/^http:\/\/localhost:\d+$/], credentials: true },
+  });
+  app.use(helmet());
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
